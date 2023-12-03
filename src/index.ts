a86c66913a027fe3f0c5d436e5f0df6f8576f84f
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-12-03 17:28:12 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,47 @@ import dotenv from "dotenv";
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN!)
 
-const WEB_APP_URL = "https://feathers.studio/telegraf/webapp/example";
+const WEB_APP_URL = "https://react-frontend-production-bb97.up.railway.app/";
 ;
 
-bot.command("inlinekb", ctx =>
+bot.command("start", ctx =>
 	ctx.reply(
 		"Launch mini app from inline keyboard!",
 		Markup.inlineKeyboard([Markup.button.webApp("Launch", WEB_APP_URL)]),
+	),
+);
+
+bot.on("inline_query", ctx =>
+	ctx.answerInlineQuery([], {
+		button: { text: "Launch", web_app: { url: WEB_APP_URL } },
+	}),
+);
+
+bot.command("link", ctx =>
+	/*
+		Go to @Botfather and create a new app for your bot first, using /newapp
+		Then modify this link appropriately.
+	
+		startapp is optional.
+		If provided, it will be passed as start_param in initData
+		and as ?tgWebAppStartParam=$command in the Web App URL
+	*/
+	ctx.reply(link("Launch", "https://t.me/$botname/$appname?startapp=$command")),
+);
+
+bot.command("setmenu", ctx =>
+	// sets Web App as the menu button for current chat
+	ctx.setChatMenuButton({
+		text: "Launch",
+		type: "web_app",
+		web_app: { url: WEB_APP_URL },
+	}),
+);
+
+bot.command("keyboard", ctx =>
+	ctx.reply(
+		"Launch mini app from keyboard!",
+		Markup.keyboard([Markup.button.webApp("Launch", WEB_APP_URL)]).resize(),
 	),
 );
 
