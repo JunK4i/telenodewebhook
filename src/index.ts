13cb0b4fff3517335ad1fb82eb63eadb33d27859
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-12-03 17:16:58 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Markup } from "telegraf";
 import dotenv from "dotenv";
 dotenv.config();
-const bot = new Telegraf(process.env.BOT_TOKEN!);
+const bot = new Telegraf(process.env.BOT_TOKEN!)
 
-bot.on("text", ctx => ctx.reply("Hello"));
+const WEB_APP_URL = "https://feathers.studio/telegraf/webapp/example";
+;
+
+bot.command("inlinekb", ctx =>
+	ctx.reply(
+		"Launch mini app from inline keyboard!",
+		Markup.inlineKeyboard([Markup.button.webApp("Launch", WEB_APP_URL)]),
+	),
+);
 
 // Start webhook via launch method (preferred)
 bot
 	.launch({ webhook: { domain: process.env.WEBHOOK_DOMAIN!, port: Number(process.env.PORT) } })
 	.then(() => console.log("Webhook bot listening on port", Number(process.env.PORT)));
+
