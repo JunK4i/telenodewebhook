7740f7f57d18958ae2a91d1d567499d29a571d6d
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-12-29 23:36:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -51,9 +51,9 @@ bot.command("keyboard", ctx =>
 	),
 );
 
-bot
-	.launch({ webhook: { domain: process.env.WEBHOOK_DOMAIN!, port: Number(process.env.PORT), hookPath: process.env.BOT_API_PATH!} })
-	.then(() => console.log("Webhook bot listening on port", Number(process.env.PORT)));
+// bot
+// 	.launch({ webhook: { domain: process.env.WEBHOOK_DOMAIN!, port: Number(process.env.PORT), hookPath: process.env.BOT_API_PATH!} })
+// 	.then(() => console.log("Webhook bot listening on port", Number(process.env.PORT)));
 
 expressApp.use(bot.webhookCallback(process.env.BOT_API_PATH!));
 bot.telegram.setWebhook(`${process.env.WEBHOOK_DOMAIN}:${process.env.PORT}/${process.env.BOT_API_PATH!}`)
